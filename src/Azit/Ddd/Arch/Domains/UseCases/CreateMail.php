aebd18ebd7c1b85844ea440d712306775ad85ee1
edythawne <itsramires@gmail.com>
2023-03-09 18:38:59 -0600

    Se agrega metodo de CC() al caso de uso de envios de correo

--- src/Azit/Ddd/Arch/Domains/UseCases/CreateMail.php
+++ src/Azit/Ddd/Arch/Domains/UseCases/CreateMail.php
@@ -16,15 +16,34 @@ class CreateMail {
         $this -> mail = Mail::to($destiny);
     }
 
-    public static function to(string $destiny) {
+    /**
+     * Destinatario
+     * @param string $destiny
+     * @return CreateMail
+     */
+    public static function to(string $destiny) : CreateMail {
         return new CreateMail($destiny);
     }
 
+    /**
+     * Se agregara la informacion que se le quiere enviar al destino por correo
+     * @param string $view
+     * @param string $subject
+     * @param array $data
+     * @return $this
+     */
     public function with(string $view, string $subject, array $data = []) : CreateMail {
         $this -> repository = new MailRepository($view, $subject, $data);
         return $this;
     }
 
+    /**
+     * Agregar documentos de tipo UploadedFile
+     * Nota: Llamar este metodo seguido de
+     * @link CreateMail::with
+     * @param array $attachments
+     * @return $this
+     */
     public function addAttachments(array $attachments) : CreateMail {
         collect($attachments) -> each(function (UploadedFile $row) {
             $this -> repository -> attach($row, ['as' =>  $row -> getClientOriginalName()]);
@@ -33,6 +52,20 @@ class CreateMail {
         return $this;
     }
 
+    /**
+     * Agregar usuarios a los que se enviaran copias del correo original
+     * @param array $users
+     * @return $this
+     */
+    public function cc(array $users): CreateMail {
+        $this -> mail -> cc($users);
+        return $this;
+    }
+
+    /**
+     * Enviar correo
+     * @return bool
+     */
     public function send() : bool {
         $response = $this -> mail -> send($this->repository);
         return isset($response);
